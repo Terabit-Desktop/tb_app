99ebd8434b1783c6f3d06b81a46d0095a651a3e9
ALEXWARELLC <123724383+ALEXWARELLC@users.noreply.github.com>
2024-03-14 11:30:04 +0800

    Added --dev-mode argument.
    Passed Basic Test.

--- core/app/index.js
+++ core/app/index.js
@@ -1,24 +1,41 @@
 const { app, BrowserWindow } = require('electron');
 const path = require('path');
+const process = require('process');
+
+var Viewport = null;
 
 const CreateViewport = () => {
     const Viewport = new BrowserWindow({
         width: 1280,
         height: 720,
-        icon: path.resolve(__dirname, "res/icon.ico")
+        icon: path.resolve(__dirname, "res/icon.ico"),
+        webPreferences: {
+            devTools: DevToolsEnabled(),
+            scrollBounce: true
+        },
     });
     Viewport.menuBarVisible = false;
     Viewport.loadURL("https://gaming.terabit.io/");
 }
 
+const DevToolsEnabled = () => {
+    if (process.argv != null)
+        if (process.argv.includes("--dev-mode"))
+            return true;
+    return false;
+}
+
 // ----- ELECTRON FUNCTIONS -----
 
 app.whenReady().then(() => {
-    CreateViewport();
+    CreateViewport(); // Create and configure the BrowserWindow settings.
 
     app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0)
+        if (BrowserWindow.getAllWindows().length === 0)    
+        {
             CreateViewport();
+            LoadWebview();
+        }
     })
 });
 
