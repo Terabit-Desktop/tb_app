8a0b84a2aad5ea3f3251963dfbf76af4d9828ab8
ɅLΞX <123724383+ALEXWARELLC@users.noreply.github.com>
2024-05-07 18:42:35 +0800

    FIx 2nd security issue.

--- src/main-process/NavigationTools.ts
+++ src/main-process/NavigationTools.ts
@@ -1,18 +1,16 @@
+import { NetUtils } from "../core/NetUtils";
 import { Log, LogLevel } from "../libs/logging/log";
 import { AppWindow } from "./AppWindow";
 
 export class NavigationTools {
     public static AddLinkRestrictions(Target: AppWindow): void {
         Target.GetWindow()?.webContents.on('will-navigate', (event, url) => {
-            const parsedUrl = new URL(url);
-            const allowedOrigins = ["discord.com", "unpkg.com"]; // Allow Discord for the Discord invite
-            const allowedBaseDomain = "terabit.io";
-
-            let CancelNavigation = !allowedOrigins.includes(parsedUrl.hostname) && !parsedUrl.hostname.endsWith(allowedBaseDomain);
-            if (CancelNavigation) {
-                Log.Write(LogLevel.WARN, `${parsedUrl} (${parsedUrl.origin}) lead to a URL outside of the allowed origin.`);
-                event.preventDefault(); // Stops any urls with origins outside terabit.io or discord.com from loading.
-            }
+            const CancelNavigation = NetUtils.IsAllowedToNavigate(url, ["terabit.io", "discord.com"]);
+            if (CancelNavigation)
+                {
+                    Log.Write(LogLevel.WARN, `Blocked navigation to ${url}`);
+                    event.preventDefault();
+                }
         });
     }
 }
