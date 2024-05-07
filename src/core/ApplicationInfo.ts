4ddb6d5968a973655b4a8403dcbe2379f7cb54b9
ɅLΞX <123724383+ALEXWARELLC@users.noreply.github.com>
2024-05-07 21:05:19 +0800

    Adjusted Verison.

--- src/core/ApplicationInfo.ts
+++ src/core/ApplicationInfo.ts
@@ -1,6 +1,6 @@
 export class ApplicationInfo {
   public static readonly Name: string = "Terabit Desktop";
-  public static readonly Version: string = "2.0.0";
+  public static readonly Version: string = "1.0.1";
   public static readonly Description: string =
     "Desktop experience for Terabit Hosting customers.";
   public static readonly Author: string = "ALEXWARELLC";
